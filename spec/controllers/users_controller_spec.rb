eeef9ddde84091874d209cc67f1232b138fa3b4c
halkity <saito75061@gmail.com>
2020-07-13 12:43:39 +0900

    rspecの準備

---
+++ spec/controllers/users_controller_spec.rb
@@ -0,0 +1,20 @@
+require 'rails_helper'
+
+describe UsersController do
+
+  describe '' do
+    context 'ログインしている場合' do
+      before do
+      end
+      # アクション内で定義しているインスタンス変数があるか
+      it '' do
+      end
+    end
+    context 'ログインしていない場合' do
+      before do
+      end
+      it '' do
+      end
+    end
+  end
+end
