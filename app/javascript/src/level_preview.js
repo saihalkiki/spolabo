1ff4001b5261f8a9cb721b6e27c66ff666cb8cb9
halkity <saito75061@gmail.com>
2020-08-19 18:25:30 +0900

    rangeの表示・非表示

--- app/javascript/src/level_preview.js
+++ app/javascript/src/level_preview.js
@@ -1,4 +1,24 @@
 $(document).on('turbolinks:load', function () {
+  $('#levelBtn').on('click', function (e) {
+    e.preventDefault();
+    $(this).prop('disabled', false);
+    if ($('#levelRange').find('.displayNone').length == 1) {
+      // ボタンをグレーにする
+      $('#levelBtn').removeClass('btn-primary');
+      $('#levelBtn').addClass('btn-secondary');
+      // 表示させて、rangeを起動させる
+      $('#levelRange').children().removeClass('displayNone');
+      $('#levelRange').find('input[type=range]').prop('disabled', false);
+    } else {
+      // ボタンをブルーにする
+      $('#levelBtn').removeClass('btn-secondary');
+      $('#levelBtn').addClass('btn-primary');
+      // 非表示させて、rangeの起動を停止する
+      $('#levelRange').children().addClass('displayNone');
+      $('#levelRange').find('input[type=range]').prop('disabled', true);
+    }
+  })
+
   /* 変更中（ドラッグ中） */
   $('input[type=range]').on('input', function () {
     $('#score').html($(this).val());
