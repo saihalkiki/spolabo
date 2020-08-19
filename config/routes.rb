a96b42c22b100b1ed41f598bfc0407b3f7fc45f8
halkity <saito75061@gmail.com>
2020-08-19 20:37:11 +0900

    ユーザーshowページを追加

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,7 @@
 Rails.application.routes.draw do
   devise_for :users, controllers: { registrations: 'users/registrations' }
   root "events#index"
+  resources :users, only: :show
   resources :events do
     resources :participations, only: [:create, :destroy]
     collection do
