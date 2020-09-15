e09359fcc72afd044e65003571828f9b6d6bb82d
halkity <saito75061@gmail.com>
2020-09-15 09:33:48 +0900

    omniauthを使うためのオプションとアソシエーションを追記

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,11 +2,12 @@ class User < ApplicationRecord
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
-  :recoverable, :rememberable, :validatable
+  :recoverable, :rememberable, :validatable, :omniauthable, omniauth_providers: [:facebook, :google_oauth2]
   
   has_many :events, class_name: 'Event', foreign_key: :owner_id, dependent: :destroy
   has_many :participations, dependent: :destroy
   has_many :join_events, through: :participations, source: :event
+  has_many :sns_credentials
   validates :name, presence: true, uniqueness: true
   mount_uploader :image, ImageUploader, dependent: :destroy
 
