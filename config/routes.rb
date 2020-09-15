173b5444ddb120b79c2721a88090fa6e20e8f637
halkity <saito75061@gmail.com>
2020-09-15 09:35:42 +0900

    deviseのルーティング

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,8 @@
 Rails.application.routes.draw do
-  devise_for :users, controllers: { registrations: 'users/registrations' }
+  devise_for :users, controllers: {
+    omniauth_callbacks: 'users/omniauth_callbacks',
+    registrations: 'users/registrations'
+  }
 
   root "events#index"
 
