d91680ee4b080653dbdbcbe974da90fa6f8fe375
halkity <saito75061@gmail.com>
2020-09-22 06:43:27 +0900

    Update users_controller.rb

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -2,4 +2,9 @@ class UsersController < ApplicationController
   def join
     @events_join = User.find_by(params[:id]).join_events
   end
+
+  def recruit
+    @events_recruit = User.find_by(params[:id]).events
+  end
+
 end
